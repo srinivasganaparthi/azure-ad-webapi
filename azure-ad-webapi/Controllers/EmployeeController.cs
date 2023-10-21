ca91671fde32f8e648d3186aefcd453daef8bfdb
Srinivas Ganaparthi <ganaparthi.srinu@gmail.com>
2023-10-21 10:54:32 -0400

    WebApi sample

---
+++ azure-ad-webapi/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAdWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetEmployees")]
+        public IEnumerable<Employee> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Employee
+            {
+                EmpId = index,
+                Name = "Employee" + index,
+                Salary = 10000
+            }).ToArray();
+        }
+    }
+}
---
+++ azure-ad-webapi/Employee.cs
@@ -0,0 +1,13 @@
+namespace AzureAdWebApi
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public int EmpId { get; set; }
+
+        public int Salary { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
